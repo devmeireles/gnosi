94fc23a9dbcc95c112073440dff9f10a0ce72795
Gabriel Meireles <gmeireles@2mundos.net>
2020-12-17 22:31:11 -0300

    Adding a pg database to the prod mode

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -8,9 +8,14 @@ const config = require(`${__dirname}/../config/database.json`)[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else if (process.env.DATABASE_URL) {
+
+if (process.env.NODE_ENV === 'development') {
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  } else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
+} else {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
@@ -18,8 +23,6 @@ if (config.use_env_variable) {
     host: `https://${process.env.SWAGGER_URL}`,
     logging: false,
   });
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
